18b043680b9a895ea4c9ce293062284b751ee9bb
Daniel Vedin <dv222bk@student.lnu.se>
2014-11-10 10:37:07 +0000

    Finished Uppgift3: birthday

--- 1-vandalen/birthday/script.js
+++ 1-vandalen/birthday/script.js
@@ -4,14 +4,31 @@ window.onload = function(){
 
 	
 	var birthday = function(date){
+		var today, dateCheck, daysToBirthday
 		
-
-
-			// Din kod här.
-
-
-
-
+		date = date.trim();
+		dateCheck = /^(19|20)[0-9]{2}-(1[0-2]|0[1-9])-(3[0-1]|[1-2][0-9]|0[1-9])$/; // Kontrollerar så att datumet är ett korrekt datum mellan 1900-01-01 och 2099-12-31 (tar inte hänsyn till att vissa månader har mindre än 31 dagar)
+		if(!dateCheck.test(date)) {
+			throw Error("Felaktigt datum format. Var god ange datum enligt YYYY-MM-DD!");
+		}
+		
+		date = new Date(date);
+		if(date.toString() === "Invalid Date") { // Kontrollerar ifall datumet är ett faktiskt korrekt datum
+			throw Error("Felaktigt datum angivet. Var god ange ett korrekt datum");
+		}
+		
+		today = new Date();
+		date.setFullYear(today.getFullYear());
+		if(today > date) { // Om födelsedagen redan har inträffat i år, så kommer nästa födelsedag att inträffa nästa år
+			date.setFullYear(date.getFullYear() + 1);
+		}
+		
+		daysToBirthday = Math.floor((date.getTime() - today.getTime()) / (1000*60*60*24)) + 1;
+		if(daysToBirthday === 365) { // Om det är 365 dagar ifrån datumet, så är det idag.
+			daysToBirthday = 0;
+		}
+		
+		return daysToBirthday;
 	};
 	// ------------------------------------------------------------------------------
 
