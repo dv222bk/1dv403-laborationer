312cbfbd7c5eb50261901ab0e5950f04e7b5e237
Daniel Vedin <dv222bk@student.lnu.se>
2014-11-10 12:41:46 +0000

    Removed my tests from vandalen script.js

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -42,8 +42,3 @@ var makePerson = function(persArr){
     
     return result; 
 } 
-var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
-
-var result = makePerson(data);
-
-console.log(result);
