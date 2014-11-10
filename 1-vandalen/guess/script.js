f28b6cd21a947f61c105bdf7224683d7dac86cf5
Daniel Vedin <dv222bk@student.lnu.se>
2014-11-10 08:03:45 +0000

    Finished Uppgift1: Gissa Det Hemliga Talet

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,21 +2,31 @@
 
 window.onload = function(){
 	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
+	var secret = Math.floor(Math.random() * 100) + 1;
+	var guesses = 0;
 	
 	// I denna funktion ska du skriva koden för att hantera "spelet"
 	var guess = function(number){
 		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
 		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
-			
-		// Plats för förändring.
-
-
-		// Returnera exempelvis: 
-		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
-		// [false, "Det hemliga talet är högre!"]
-		// [false, "Det hemliga talet är lägre!"]
-		// [false, "Talet är utanför intervallet 0 - 100"]		
+		
+		number = +number;
+		
+		guesses += 1;
+		
+		if (number === secret) {
+			return [true, "Grattis du vann! Det hemliga talet var " + secret + " och du behövde " + guesses + " gissningar för att hitta det."];
+		} else if (number < 0 || number > 100) {
+			return [false, "Talet är utanför intervallet 0 - 100"];
+		} else if (number < secret) {
+			return [false, "Det hemliga talet är högre!"];
+		} else if (number > secret) {
+			return [false, "Det hemliga talet är lägre!"];
+		}
+		
+		guesses -= 1; // Om värdet inte var en giltlig gissning, räkna den inte
+		
+		return [false, "Det inmatade värdet är inte ett nummer"];
 	};
 	
 	// ------------------------------------------------------------------------------
