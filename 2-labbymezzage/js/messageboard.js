ea13557d17ab5fd5a07cc22517ed22f5ea0f9dd6
Daniel Vedin <dv222bk@student.lnu.se>
2014-11-25 22:09:28 +0000

    Rewrote exisiting code for star-quality

--- 2-labbymezzage/js/messageboard.js
+++ 2-labbymezzage/js/messageboard.js
@@ -1,9 +1,61 @@
 "use strict";
 
-var MessageBoard = {
-    "messages" : [],
+window.onload = function() {
+        var what = new MessageBoard("msg1");
+        what.pushMessage("what what");
+        alert(what.getMessages());
+}
+
+function MessageBoard(elementID) {
+    var section, div, header, p, button, element;
+    var that = this;
+    var messages = [];
     
-    "init" : window.onload = function() {
-        var mess = new Message("Testmeddelande", new Date());
+    this.getMessages = function() {
+        return messages;
     }
-};
+    
+    this.getMessage = function(index) {
+        return messages[index];
+    }
+    
+    this.pushMessage = function(_message) {
+        messages.push(new Message(_message, new Date()));
+    }
+    
+    /* App DOM */
+    element = document.getElementById(elementID);
+    
+    /* Section */
+    section = document.createElement("section");
+    section.className = "LabbyMezzage";
+    element.appendChild(section);
+    
+    /* Holder div */
+    div = document.createElement("div");
+    section.appendChild(div);
+    
+    /* Header */
+    header = document.createElement("header");
+    p = document.createElement("p");
+    p.innerHTML = "v0.1b";
+    header.appendChild(p);
+    div.appendChild(header);
+    
+    /* Textarea */
+    div.appendChild(document.createElement("textarea"));
+    
+    /* Send-button */
+    button = document.createElement("button");
+    button.type = "submit";
+    button.innerHTML = "Skriv";
+    button.onclick = function(e) {
+        that.sendMessage();
+        return false;
+    }
+    div.appendChild(button);
+}
+
+MessageBoard.prototype.sendMessage = function() {
+    
+}
