701f015814cfcb74b5a71a2f9fbcede21d6a2661
Daniel Vedin <dv222bk@student.lnu.se>
2014-11-26 19:41:10 +0000

    Fixed linebreak bug

--- 2-labbymezzage/js/message.js
+++ 2-labbymezzage/js/message.js
@@ -24,7 +24,7 @@ Message.prototype.toString = function() {
 };
 
 Message.prototype.getHTMLText = function() {
-    return this.getText().replace(/\n\r/g, "<br />");
+    return this.getText().replace(/\n/g, "<br />");
 };
 
 Message.prototype.getDateText = function() {
