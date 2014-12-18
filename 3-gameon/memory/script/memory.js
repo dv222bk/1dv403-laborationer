15da25bb140784a8870e98612f2a3942923961cf
Daniel Vedin <dv222bk@student.lnu.se>
2014-12-18 11:16:36 +0000

    Continued working on memory.js

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -1,8 +1,12 @@
 function MemoryGame(elementID, rows, cols) {
+    var pictureArray, flippedImages, tries;
+    
     /* root element */
     this.root = document.getElementById(elementID);
     
-    var pictureArray = RandomGenerator.getPictureArray(rows, cols);
+    pictureArray = RandomGenerator.getPictureArray(rows, cols);
+    flippedImages = 0;
+    tries = 0;
     
     this.getPictureArray = function() {
         return pictureArray;
@@ -10,17 +14,33 @@ function MemoryGame(elementID, rows, cols) {
     
     this.getRows = function() {
         return rows;
-    }
+    };
     
     this.getCols = function() {
         return cols;
+    };
+    
+    this.getFlippedImages = function() {
+        return flippedImages;
+    }
+    
+    this.setFlippedImages = function(number) {
+        flippedImages = number;
+    }
+    
+    this.getTries = function() {
+        return tries;
+    }
+    
+    this.setTries = function(number) {
+        tries = number;
     }
 
     this.createApp();
 }
 
 MemoryGame.prototype.createApp = function() {
-    var appBody, div, header, appVersion, col, row, rowDiv, memoryImage;
+    var appBody, header, appVersion, col, row, rowDiv, memoryImage;
     var that = this;
     
     /* Section */
@@ -36,16 +56,29 @@ MemoryGame.prototype.createApp = function() {
     appBody.appendChild(header);
     
     /* Memory images */
-    for(row = 0; row < this.getRows; col+=1) {
+    for(row = 0; row < this.getRows(); col+=1) {
         rowDiv = document.createElement("div");
         
-        for(col = 0; col < this.getCols; col+=1) {
+        for(col = 0; col < this.getCols(); col+=1) {
             memoryImage = document.createElement("img");
             memoryImage.alt = "Ett memory kort";
             memoryImage.title = "Klicka för att se vilken bild som gömmer sig bakom det här kortet!";
             memoryImage.src = "pics/0.png";
+            memoryImage.onclick = function(e) {
+                e.preventDefault();
+                that.checkImage(row, col);
+                return false;
+            };
             rowDiv.appendChild(memoryImage);
         }
         appBody.appendChild(rowDiv);
     }
+};
+
+MemoryGame.prototype.checkImage = function(row, col) {
+    var memoryImage;
+    memoryImage = this.root.querySelector("div")[row].childNodes()[col];
+    memoryImage.src = "pics/" + ((row * this.getCols()) + col) + ".png";
+    this.setFlippedImages(this.getFlippedImages + 1);
+    this.checkStatus();
 };
