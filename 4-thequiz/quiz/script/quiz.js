0490729795e4743623e889191ad5f4f15f304661
Daniel Vedin <dv222bk@student.lnu.se>
2015-01-05 13:27:28 +0000

    Added code for recieveing questions and answering questions. The answering does not quite work yet

--- 4-thequiz/quiz/script/quiz.js
+++ 4-thequiz/quiz/script/quiz.js
@@ -1,16 +1,25 @@
 "use strict";
 
-function QuizGame(elementID) {
-    var that = this;
+function QuizGame(elementID, firstURL) {
+    var nextURL = firstURL;
     
     /* root element */
     this.root = document.getElementById(elementID);
     
+    this.setNextURL = function(URL) {
+        nextURL = URL; 
+    }
+    
+    this.getNextURL = function() {
+        return nextURL;
+    }
+    
     this.createApp();
+    this.recieveQuestion();
 }
 
 QuizGame.prototype.createApp = function() {
-    var appBody, header, appName;
+    var appBody, header, appName, questionHolder, questionP, answerInput, submitButton;
     var that = this;
     
     /* Section */
@@ -25,4 +34,66 @@ QuizGame.prototype.createApp = function() {
     header.appendChild(appName);
     appBody.appendChild(header);
     
+    /* Question holder */
+    questionHolder = document.createElement("div");
+    questionP = document.createElement("p");
+    questionP.className = "question";
+    questionHolder.appendChild(questionP);
+    appBody.appendChild(questionHolder);
+    
+    /* Input field */
+    answerInput = document.createElement("input");
+    answerInput.type = "text";
+    answerInput.name = "answer";
+    appBody.appendChild(answerInput);
+    
+    /* Send-button */
+    submitButton = document.createElement("button");
+    submitButton.type = "submit";
+    submitButton.innerHTML = "Svara";
+    submitButton.onclick = function(e) {
+        e.preventDefault();
+        that.sendAnswer(that.root.querySelector("input").value);
+        return false;
+    };
+    appBody.appendChild(submitButton);
+};
+
+QuizGame.prototype.sendAnswer = function(answer) {
+    var xhr, JSONData;
+    var that = this;
+    xhr = new XMLHttpRequest();
+    xhr.abort();
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState === 4) {
+            if(xhr.status === 200) {
+                console.log(xhr.responseText);
+            } else {
+                console.log("Läsfel. Status: " + xhr.status);
+            }
+        }
+    };
+    xhr.open("POST", this.getNextURL(), true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(answer));
+};
+
+QuizGame.prototype.recieveQuestion = function() {
+    var xhr, JSONData;
+    var that = this;
+    xhr = new XMLHttpRequest();
+    xhr.abort();
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState === 4) {
+            if(xhr.status === 200) {
+                JSONData = JSON.parse(xhr.responseText); 
+                that.root.querySelector(".question").innerHTML = JSONData.question;
+                that.setNextURL(JSONData.nextURL);
+            } else {
+                console.log("Läsfel. Status: " + xhr.status);
+            }
+        }
+    };
+    xhr.open("GET", this.getNextURL(), true);
+    xhr.send(null);
 };
--- 4-thequiz/quiz/script/script.js
+++ 4-thequiz/quiz/script/script.js
@@ -1,5 +1,5 @@
 "use strict";
 
 window.onload = function() {
-    new QuizGame("quizGame");
+    new QuizGame("quizGame", "http://vhost3.lnu.se:20080/question/1");
 };
