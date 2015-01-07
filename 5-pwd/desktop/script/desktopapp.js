ceb876a427b1d7df266cd5f29d3b96c882042a45
Daniel Vedin <dv222bk@student.lnu.se>
2015-01-07 15:08:18 +0000

    Added an app icon to the desktoptoolbar

--- 5-pwd/desktop/script/desktopapp.js
+++ 5-pwd/desktop/script/desktopapp.js
@@ -10,7 +10,7 @@ function DesktopApp(elementID) {
 }
 
 DesktopApp.prototype.createApp = function() {
-    var appbody, toolbar;
+    var appbody, toolbar, appLink, imageViewerApp;
     
     /* AppBody */
     appbody = document.createElement("section");
@@ -21,6 +21,16 @@ DesktopApp.prototype.createApp = function() {
     toolbar = document.createElement("div");
     toolbar.className = "desktopToolbar";
     appbody.appendChild(toolbar);
+    
+    /* Image Viewer App */
+    appLink = document.createElement("a");
+    imageViewerApp = document.createElement("img");
+    imageViewerApp.alt = "Klicka för att starta ImageViewer appen";
+    imageViewerApp.title = "ImageViewer";
+    imageViewerApp.src = "desktop/pics/appIcons/imageViewer.png";
+    imageViewerApp.className = "appIcon";
+    appLink.appendChild(imageViewerApp);
+    toolbar.appendChild(appLink);
 };
 
 DesktopApp.prototype.createWindow = function() {
