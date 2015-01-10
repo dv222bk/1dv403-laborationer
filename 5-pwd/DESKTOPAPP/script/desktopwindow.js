12a4eedfca2efcdab78a3d50c8105a428d6e4514
Daniel Vedin <dv222bk@student.lnu.se>
2015-01-10 13:47:57 +0000

    More work to make windows drag and droppable. Added a top to the windowHeader, where the event will go off

--- 5-pwd/DESKTOPAPP/script/desktopwindow.js
+++ 5-pwd/DESKTOPAPP/script/desktopwindow.js
@@ -3,16 +3,44 @@
 var DESKTOPAPP = DESKTOPAPP || {};
 
 DESKTOPAPP.DesktopWindow = function() {
+    /* Move Window variables */
+    var startX, startY, currentX, currentY;
+    
     this.windowBody;
     this.windowHolder;
     this.contextMenu;
     this.contextMenuArkiv;
     this.statusField;
     this.desktop;
+    
+    this.getStartX = function() {
+        return startX;
+    };
+    this.setStartX = function(xCord) {
+        startX = xCord;
+    };
+    this.getStartY = function() {
+        return startY;
+    };
+    this.setStartY = function(yCord) {
+        startY = yCord;
+    };
+    this.getCurrentX = function() {
+        return currentX;
+    };
+    this.setCurrentX = function(xCord) {
+        currentX = xCord;
+    };
+    this.getCurrentY = function() {
+        return currentY;
+    };
+    this.setCurrentY = function(yCord) {
+        currentY = yCord;
+    };
 };
 
 DESKTOPAPP.DesktopWindow.prototype.createWindow = function(desktop, width, height) {
-    var windowHeader, appIcon, appName, closeButton, contextMenuArkivMenu, contextMenuArkivMenuClose, contextMenuArkivMenuCloseA, contextMenuArkivMenuCloseImg;
+    var windowHeader, windowHeaderTop, appIcon, appName, closeButton, contextMenuArkivMenu, contextMenuArkivMenuClose, contextMenuArkivMenuCloseA, contextMenuArkivMenuCloseImg;
     var that = this;
     
     this.desktop = desktop;
@@ -35,17 +63,29 @@ DESKTOPAPP.DesktopWindow.prototype.createWindow = function(desktop, width, heigh
     windowHeader.className = "windowHeader";
     this.windowHolder.appendChild(windowHeader);
     
+    /* WindowHeader Top */
+    windowHeaderTop = document.createElement("div");
+    windowHeaderTop.onmousedown = function(e) {
+        document.addEventListener("mousemove", that.moveWindow(e), false);
+    };
+    windowHeaderTop.onmouseup = function(e) {
+        e.preventDefault();
+        document.removeEventListener("mousemove", that.moveWindow(e), false);
+        return false;
+    };
+    windowHeader.appendChild(windowHeaderTop);
+    
     /* App Icon */
     appIcon = document.createElement("img");
     appIcon.alt = "Iconen för " + desktop.apps[this.app].name;
     appIcon.title = desktop.apps[this.app].name;
     appIcon.src = desktop.apps[this.app].iconURL;
-    windowHeader.appendChild(appIcon);
+    windowHeaderTop.appendChild(appIcon);
     
     /* App name */
     appName = document.createElement("p");
     appName.innerHTML = desktop.apps[this.app].name;
-    windowHeader.appendChild(appName);
+    windowHeaderTop.appendChild(appName);
     
     /* Close button */
     closeButton = document.createElement("button");
@@ -56,7 +96,7 @@ DESKTOPAPP.DesktopWindow.prototype.createWindow = function(desktop, width, heigh
         that.closeWindow();
         return false;
     };
-    windowHeader.appendChild(closeButton);
+    windowHeaderTop.appendChild(closeButton);
     
     /* Context Menu */
     this.contextMenu = document.createElement("ul");
@@ -120,6 +160,27 @@ DESKTOPAPP.DesktopWindow.prototype.createWindow = function(desktop, width, heigh
 DESKTOPAPP.DesktopWindow.prototype.closeWindow = function() {
     this.desktop.removeWindow(this.windowHolder);
     this.windowHolder.parentNode.removeChild(this.windowHolder);
+};
+
+DESKTOPAPP.DesktopWindow.prototype.moveWindow = function(e) {
+    /* http://www.quirksmode.org/js/events_properties.html#position */
+    var posx = 0;
+	var posy = 0;
+	if (!e) e = window.event;
+	if (e.pageX || e.pageY) 	{
+		posx = e.pageX;
+		posy = e.pageY;
+	}
+	else if (e.clientX || e.clientY) 	{
+		posx = e.clientX + document.body.scrollLeft
+			+ document.documentElement.scrollLeft;
+		posy = e.clientY + document.body.scrollTop
+			+ document.documentElement.scrollTop;
+	}
+	this.setStartX(posx);
+	this.setStartY(posy);
+	console.log(posx);
+	console.log(posy);
 };
 
 DESKTOPAPP.DesktopWindow.prototype.showLoading = function() {
